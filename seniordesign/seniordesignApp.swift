69c147faea537da69f8bd24d0d470e5c3eca3ead
pganchozo <pganchoz@bu.edu>
2020-10-26 19:21:12 -0400

    Initial Commit

---
+++ seniordesign/seniordesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  seniordesignApp.swift
+//  seniordesign
+//
+//  Created by Patricia  Ganchozo on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct seniordesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
