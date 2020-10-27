0825b571c38eca67350597512411f5c6ffda52d7
pganchozo <pganchoz@bu.edu>
2020-10-26 22:33:48 -0400

    welcome page

--- seniordesign/ContentView.swift
+++ seniordesign/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingSheet = false
+
+      var body: some View {
+        
+        Text("WELCOME").font(.system(size:42, weight: .heavy, design: .rounded))
+                .padding()
+          
+        .onAppear(perform: delayText)
+        .sheet(isPresented: $showingSheet) {
+            secondview()
+        }
+      }
+    
+    private func delayText() {
+        // Delay of 3.5 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+            showingSheet = true
+        }
+    }
+}
+
+
+struct secondview: View {
+    
+    @State private var isScan = false
     var body: some View {
-        Text("WELCOME")
-            .padding()
+        Button("SCAN") {
+            self.isScan.toggle()
+        }
     }
 }
 
