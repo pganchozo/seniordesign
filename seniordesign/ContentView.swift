a715d540cfc6fba875649326b16ea983f287c67c
pganchozo <pganchoz@bu.edu>
2020-10-27 15:29:28 -0400

    navigation link

--- seniordesign/ContentView.swift
+++ seniordesign/ContentView.swift
@@ -12,8 +12,13 @@ struct ContentView: View {
 
       var body: some View {
         
-        Text("WELCOME").font(.system(size:42, weight: .heavy, design: .rounded))
-                .padding()
+        ZStack {
+            Color.blue
+            .edgesIgnoringSafeArea(.all)
+            Text("WELCOME").font(.system(size:42, weight: .heavy, design: .rounded))
+                    .padding()
+        }
+        
           
         .onAppear(perform: delayText)
         .sheet(isPresented: $showingSheet) {
@@ -33,23 +38,51 @@ struct ContentView: View {
 struct secondview: View {
     
     @State private var isScan = false
+    @State private var isNavigation = false
+    
     var body: some View {
-        Button("SCAN") {
-            self.isScan.toggle()
+        
+        NavigationView {
+            HStack {
+                VStack{
+                    NavigationLink(destination: scanView()) {
+                        Text("SCAN")
+                    }
+                }
+                VStack{
+                    NavigationLink(destination: gpsView()) {
+                        Text("GPS")
+                    }
+                }
+            }
         }
         
-//        if isScan {
-//            thirdview()
+//        Button("SCAN") {
+//            self.isScan.toggle()
+//        }
+//        .sheet(isPresented: $isScan) {
+//            scanView()
+//        }
+//
+//        Button("NAVIGATION"){
+//            self.isNavigation.toggle()
+//        }
+//        .sheet(isPresented: $isNavigation) {
+//            navView()
 //        }
     }
     
-    
 }
 
-struct thirdview: View {
-    
+struct scanView: View {
     var body: some View {
-        Text("yay")
+        Text("SCAN STUFF")
+    }
+}
+
+struct gpsView: View {
+    var body: some View {
+        Text("GPS STUFF")
     }
 }
 
