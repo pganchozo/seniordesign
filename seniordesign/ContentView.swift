69042793298781b654ad20d8ff0a632c288e5f57
pganchozo <pganchoz@bu.edu>
2020-10-28 19:00:34 -0400

    Added Camera Function

--- seniordesign/ContentView.swift
+++ seniordesign/ContentView.swift
@@ -22,69 +22,20 @@ struct ContentView: View {
           
         .onAppear(perform: delayText)
         .sheet(isPresented: $showingSheet) {
-            secondview()
+            secondView()
         }
       }
     
     private func delayText() {
         // Delay of 3.5 seconds
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             showingSheet = true
         }
     }
 }
 
 
-struct secondview: View {
-    
-    @State private var isScan = false
-    @State private var isNavigation = false
-    
-    var body: some View {
-        
-        NavigationView {
-            HStack {
-                VStack{
-                    NavigationLink(destination: scanView()) {
-                        Text("SCAN")
-                    }
-                }
-                VStack{
-                    NavigationLink(destination: gpsView()) {
-                        Text("GPS")
-                    }
-                }
-            }
-        }
-        
-//        Button("SCAN") {
-//            self.isScan.toggle()
-//        }
-//        .sheet(isPresented: $isScan) {
-//            scanView()
-//        }
-//
-//        Button("NAVIGATION"){
-//            self.isNavigation.toggle()
-//        }
-//        .sheet(isPresented: $isNavigation) {
-//            navView()
-//        }
-    }
-    
-}
 
-struct scanView: View {
-    var body: some View {
-        Text("SCAN STUFF")
-    }
-}
-
-struct gpsView: View {
-    var body: some View {
-        Text("GPS STUFF")
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ seniordesign/Views/secondView.swift
@@ -0,0 +1,40 @@
+//
+//  secondView.swift
+//  seniordesign
+//
+//  Created by Patricia  Ganchozo on 10/28/20.
+//
+
+import SwiftUI
+
+struct secondView: View {
+    
+    @State private var isScan = false
+    @State private var isNavigation = false
+    
+    var body: some View {
+        
+        NavigationView {
+            HStack {
+                VStack{
+                    NavigationLink(destination: scanView().edgesIgnoringSafeArea(.top)) {
+                        Text("SCAN")
+                    }
+                }
+                VStack{
+                    NavigationLink(destination: gpsView()) {
+                        Text("GPS")
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+
+struct secondView_Previews: PreviewProvider {
+    static var previews: some View {
+        secondView()
+    }
+}
